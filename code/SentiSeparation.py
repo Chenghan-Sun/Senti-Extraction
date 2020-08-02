739007e7978b20de7ee5a4458d44369941fe74b4
TinnyZhu <60484412+TinnyZhu@users.noreply.github.com>
2020-08-02 15:28:41 -0700

    Separate data into different files by their sentiment

---
+++ code/SentiSeparation.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from typing import Dict
+import numpy as np
+
+
+class SentiSeparation(object):
+    def __init__(self, pd_df: pd.DataFrame) -> None:
+        self.Pos=self.data[self.data['sentiment']=='positive']
+        self.Neg=self.data[self.data['sentiment']=='negative']
+        self.Neu=self.data[self.data['sentiment']=='neutral']
+        self.Pos.to_csv(r'../data/Positive.csv')
+        self.Neg.to_csv(r'../data/Negative.csv')
+        self.Neu.to_csv(r'../data/Neutral.csv')
