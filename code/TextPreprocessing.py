337e17c436430e34443b6194446b28a7fa5ea4c9
Chenghan Sun <sunchenghan1995@gmail.com>
2020-08-02 16:01:07 -0700

    init text processing class

---
+++ code/TextPreprocessing.py
@@ -0,0 +1,49 @@
+""" Class for Tweets Preprocessing
+"""
+
+import re
+from nltk.tokenize import word_tokenize
+
+
+class TweetPreprocess(object):
+    """ Tweets Preprocessing class including additional methods for analysis
+    Currently only support analysis for a piece of single tweet
+    """
+    
+    def __init__(self, tweet):
+        self.tweet = tweet
+        
+    def process_tweet(self, ):
+        """ Ensemble methods for single tweet processing 
+        """
+        words = word_tokenize(self.tweet)
+        words = tweet.split()
+        for word in words:
+            clear_word = _word_process(word)
+        return clear_word
+    
+    def _detect_tweet(self, ):
+        """ 
+        """
+        pass 
+
+    def _word_process(self, word):
+        """ pre-processing each word into unified type
+        """
+        # Remove all kinds of punctuation
+        clear_word = word.strip('\'"?!,.():;')
+
+        # Convert more than 2 letter repetitions to 2 letter
+        clear_word = re.sub(r'(.)\1+', r'\1\1', clear_word)
+
+        # Remove - & '
+        clear_word = re.sub(r'(-|\')', '', clear_word)
+        
+        # check validation of the processed word 
+        word_flag = re.search(r'^[a-zA-Z][a-z0-9A-Z\._]*$', clear_word)
+        if word_flag:
+            return clear_word
+        else:
+            return None
+        
+        
